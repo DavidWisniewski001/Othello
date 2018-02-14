54fd0add11260348531905e9e3fb0ccf63a415bd
david_wisniewski <david.wisniewski.work@gmail.com>
2018-02-14 17:16:14 -0500

    This is a much more simple Makefile

---
+++ makefile.mk
@@ -0,0 +1,29 @@
+################################################################################
+# David Wisniewski
+# Homework5 Make Files
+#
+#
+#
+build: Game
+
+Game: main.o Myothello.o space.o game.o
+	clang++ main.o Myothello.o space.o game.o -o Othello
+
+main.o: main.cc
+	clang++ -c main.cc
+
+game.o: game.cc
+	clang++ -c game.cc
+
+Myothello.o: Myothello.cc
+	clang++ -c Myothello.cc
+
+space.o: space.cc
+	clang++ -c space.cc
+
+clean:
+	rm -f *.o core *.core
+	rm MyOthello
+
+archive: main.cc game.cc Myothello.cc space.cc char.h  colors.h game.h space.h Myothello.h makefile.mk
+	tar cvzf *.o Game
